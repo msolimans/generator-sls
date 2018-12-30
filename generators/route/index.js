ea9aac89eaf6d0a673c97cb62568f83929f5aad3
Muhammad Soliman <msuliman@mum.edu>
2018-12-30 13:52:41 -0500

    main route, restructure

--- generators/route/index.js
+++ generators/route/index.js
@@ -5,6 +5,8 @@ const generators = require('yeoman-generator');
 const fileReader = require('html-wiring');
 const ucfirst = require('ucfirst');
 
+
+
 /**
  * Generates the different types of names for our routes
  * @param {String} routeName Route Name
@@ -26,42 +28,42 @@ const genRoutesNames = (routeName) => {
  * @param  {String} file String representation of our file
  * @return {String} Our modified version of the input file
  */
-function updateYamlFile(route, file) {
-    const hook = '### yeoman hook ###';
-    let newFile = null;
-    const insert = `  ${route.slugName}:\n` +
-        `    handler: bin/${route.slugName}\n` +
-        '    events:\n' +
-        '      - http:\n' +
-        `          path: ${route.slugName}\n` +
-        `          method: ${route.method}\n` +
-        '          cors: true\n';
-
-    // events:
-    //     - schedule: cron(0/2 * ? * MON-FRI *)
-    //every 2nd minute from Monday to Friday
-    //rate(value unit) (minute minutes hour hours day days)
-    //cron(Minutes Hours Day-of-month Month Day-of-week Year)
-    //https://github.com/serverless/examples/tree/master/aws-node-scheduled-cron
-
-    if (file.indexOf(insert) === -1) {
-        newFile = file.replace(hook, insert + hook);
-    }
-
-    return newFile;
-}
-
-function updateMakeFile(route, file) {
-    const hook = '### yeoman hook ###';
-    let newFile = null;
-    const insert = `	GOARCH=amd64 GOOS=linux go build -gcflags='-N -l' -o bin/${route.slugName} ${route.slugName}/main.go\n`;
-
-    if (file.indexOf(insert) === -1) {
-        newFile = file.replace(hook, insert + hook);
-    }
-
-    return newFile;
-}
+// function updateYamlFile(route, file) {
+//     const hook = '### yeoman hook ###';
+//     let newFile = null;
+//     const insert = `  ${route.slugName}:\n` +
+//         `    handler: bin/${route.slugName}\n` +
+//         '    events:\n' +
+//         '      - http:\n' +
+//         `          path: ${route.slugName}\n` +
+//         `          method: ${route.method}\n` +
+//         '          cors: true\n';
+//
+//     // events:
+//     //     - schedule: cron(0/2 * ? * MON-FRI *)
+//     //every 2nd minute from Monday to Friday
+//     //rate(value unit) (minute minutes hour hours day days)
+//     //cron(Minutes Hours Day-of-month Month Day-of-week Year)
+//     //https://github.com/serverless/examples/tree/master/aws-node-scheduled-cron
+//
+//     if (file.indexOf(insert) === -1) {
+//         newFile = file.replace(hook, insert + hook);
+//     }
+//
+//     return newFile;
+// }
+//
+// function updateMakeFile(route, file) {
+//     const hook = '### yeoman hook ###';
+//     let newFile = null;
+//     const insert = `	GOARCH=amd64 GOOS=linux go build -gcflags='-N -l' -o bin/${route.slugName} ${route.slugName}/main.go\n`;
+//
+//     if (file.indexOf(insert) === -1) {
+//         newFile = file.replace(hook, insert + hook);
+//     }
+//
+//     return newFile;
+// }
 
 
 /**
@@ -135,54 +137,63 @@ const serverGenerator = generators.Base.extend({
             const makePath = this.destinationPath('Makefile');
             let makeFile = fileReader.readFileAsString(makePath);
 
-            // We process each route
-            this.routes.forEach((route) => {
-                // We check the route doesn't already exists
-                if (
-                    this.fs.exists(this.destinationPath(`${route.slugName}/main.go`))
-                ) {
-                    this.log(`Route ${route.slugName} already exists`);
-                } else {
-
-                    const root = `./../../route/${slsAttributes.language}/`;
-
-                    this.fs.copyTpl(
-                        this.templatePath(`${root}/main.go`),
-                        this.destinationPath(`${route.slugName}/main.go`)
-                    );
-
-                    //unit test
-                    this.fs.copyTpl(
-                        this.templatePath(`${root}/main_test.go`),
-                        this.destinationPath(`${route.slugName}/main_test.go`)
-                    );
-
-                    //events
-                    this.fs.copyTpl(
-                        this.templatePath(`${root}/event.json`),
-                        this.destinationPath(`${route.slugName}/event.json`)
-                    );
-
-                    file = updateYamlFile(route, file);
-                    makeFile = updateMakeFile(route, makeFile);
-                }
-
-
-                this.fs.copyTpl(
-                    this.templatePath(`\`${root}/Makefile`),
-                    this.destinationPath(`${route.slugName}/Makefile`),
-                    {
-                        routeName: route.slugName
+            this.composeWith(`sls:r${slsAttributes.language}`,
+                {
+                    options: {
+                        __app: this.options.__app,
+                        language: slsAttributes.language,
+                        routes: this.routes
                     }
-                );
+                });
 
-            });
-
-            // rewrite the serverless.yml
-            this.write(path, file);
-
-            //Makefile
-            this.write(makePath, makeFile);
+            // // We process each route
+            // this.routes.forEach((route) => {
+            //     // We check the route doesn't already exists
+            //     if (
+            //         this.fs.exists(this.destinationPath(`${route.slugName}/main.go`))
+            //     ) {
+            //         this.log(`Route ${route.slugName} already exists`);
+            //     } else {
+            //
+            //         const root = `./../../route/${slsAttributes.language}/`;
+            //
+            //         this.fs.copyTpl(
+            //             this.templatePath(`${root}/main.go`),
+            //             this.destinationPath(`${route.slugName}/main.go`)
+            //         );
+            //
+            //         //unit test
+            //         this.fs.copyTpl(
+            //             this.templatePath(`${root}/main_test.go`),
+            //             this.destinationPath(`${route.slugName}/main_test.go`)
+            //         );
+            //
+            //         //events
+            //         this.fs.copyTpl(
+            //             this.templatePath(`${root}/event.json`),
+            //             this.destinationPath(`${route.slugName}/event.json`)
+            //         );
+            //
+            //         file = updateYamlFile(route, file);
+            //         makeFile = updateMakeFile(route, makeFile);
+            //     }
+            //
+            //
+            //     this.fs.copyTpl(
+            //         this.templatePath(`\`${root}/Makefile`),
+            //         this.destinationPath(`${route.slugName}/Makefile`),
+            //         {
+            //             routeName: route.slugName
+            //         }
+            //     );
+            //
+            // });
+            //
+            // // rewrite the serverless.yml
+            // this.write(path, file);
+            //
+            // //Makefile
+            // this.write(makePath, makeFile);
         },
     },
     sls2sam() {
