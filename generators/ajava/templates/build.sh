3ee55d87fdbe6453aee5fd46c9bce6674c79d83b
Muhammad Soliman <msuliman@mum.edu>
2019-01-11 15:16:19 -0500

    build/gradle
    Signed-off-by: Muhammad Soliman <msuliman@mum.edu>

---
+++ generators/ajava/templates/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#install zip on debian OS, since microsoft/dotnet container doesn't have zip by default
+if [ -f /etc/debian_version ]
+then
+  apt -qq update
+  apt -qq -y install zip
+fi
+
+gradle build
