98a64aaa3c34f6b40423859d3278205f3b239b57
Muhammad Soliman <msuliman@mum.edu>
2019-02-03 21:00:52 -0500

    coding style, codacy
    Signed-off-by: Muhammad Soliman <msuliman@mum.edu>

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
-const path = require('path');
-const yosay = require('yosay');
-const to = require('to-case');
-const generators = require('yeoman-generator');
+const path = require("path");
+const yosay = require("yosay");
+const to = require("to-case");
+const generators = require("yeoman-generator");
 
 /**
  * Our main generator
@@ -12,53 +12,53 @@ const serverGenerator = generators.Base.extend({
     prompting: {
         welcome() {
             this.log(yosay(
-                'Hello there, let\'s create a serverless architecture together!'
+                "Hello there, let\"s create a serverless architecture together!"
             ));
         },
 
         ask() {
             return this.prompt([
                 {
-                    name: 'language',
-                    type: 'list',
-                    message: 'What is the runtime language?',
+                    name: "language",
+                    type: "list",
+                    message: "What is the runtime language?",
                     choices: [
-                        'Golang',
-                        'NodeJS',
-                        'DotNet',
-                        'Python',
-                        'Java'
+                        "Golang",
+                        "NodeJS",
+                        "DotNet",
+                        "Python",
+                        "Java"
                     ],
-                    default: 'Golang'
+                    default: "Golang"
                 }, {
-                    name: 'projectName',
-                    type: 'input',
-                    message: 'What is the project name:',
+                    name: "projectName",
+                    type: "input",
+                    message: "What is the project name:",
                     filter: answer => to.slug(answer),
                     default: path.basename(this.destinationPath()),
                 }, {
-                    name: 'projectOwner',
-                    type: 'input',
-                    message: 'What is the git project owner (owner/repository):',
+                    name: "projectOwner",
+                    type: "input",
+                    message: "What is the git project owner (owner/repository):",
                     filter: answer => to.slug(answer)
                 }, {
-                    name: 'projectDescription',
-                    type: 'input',
-                    message: 'Enter the project description:',
+                    name: "projectDescription",
+                    type: "input",
+                    message: "Enter the project description:",
                 }, {
-                    name: 'projectVersion',
-                    type: 'input',
-                    message: 'Version:',
-                    default: '0.1.0',
+                    name: "projectVersion",
+                    type: "input",
+                    message: "Version:",
+                    default: "0.1.0",
                 }, {
-                    name: 'authorName',
-                    type: 'input',
-                    message: 'Author name:',
+                    name: "authorName",
+                    type: "input",
+                    message: "Author name:",
                     store: true,
                 }, {
-                    name: 'authorEmail',
-                    type: 'input',
-                    message: 'Author email: ',
+                    name: "authorEmail",
+                    type: "input",
+                    message: "Author email: ",
                     store: true,
                 },
             ]).then((answers) => {
@@ -80,14 +80,14 @@ const serverGenerator = generators.Base.extend({
     },
     scripts() {
         this.fs.copy(
-            this.templatePath(`./../../app/templates/scripts`),
-            this.destinationPath('scripts')
-        )
+            this.templatePath("./../../app/templates/scripts"),
+            this.destinationPath("scripts")
+        );
     },
 
     end() {
-        //this.spawnCommand("sls", ['plugin', 'install', '--name', 'serverless-sam']);
-        //this.spawnCommand('sls', ['sam', 'export', ' --output', 'template.yml']);
+        //this.spawnCommand("sls", ["plugin", "install", "--name", "serverless-sam"]);
+        //this.spawnCommand("sls", ["sam", "export", " --output", "template.yml"]);
 
         this.log(
             yosay(`Your ${this.language} project has been successfully set up, Happy Coding!!`)
