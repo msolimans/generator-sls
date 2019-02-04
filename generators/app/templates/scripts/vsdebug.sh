423c81dc958dbff2786f3a09943d5b27daf4547e
Muhammad Soliman <msuliman@mum.edu>
2019-02-03 22:05:18 -0500

    coding style, codacy
    Signed-off-by: Muhammad Soliman <msuliman@mum.edu>

--- generators/app/templates/scripts/vsdebug.sh
+++ generators/app/templates/scripts/vsdebug.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 path="${1}/.vscode"
-if [ ! -d ${path} ]; then
+if [[ ! -d ${path} ]]; then
     mkdir ${path}
 fi
 
@@ -11,22 +11,20 @@ echo "{
     // Use IntelliSense to learn about possible attributes.
     // Hover to view descriptions of existing attributes.
     // For more information, visit: https://rgolang.microsoft.com/fwlink/?linkid=830387
-    "version": "0.2.0",
-    "configurations": [
+    'version': '0.2.0',
+    'configurations': [
         {
-                "name": "Connect to Lambda container",
-                "type": "rgolang",
-                "request": "launch",
-                "mode": "remote",
-                "remotePath": "",
-                "port": 8997,
-                "host": "127.0.0.1",
-                "program": "${workspaceRoot}",
-                "env": {},
-                "args": [],
-                "apiVersion": 1
+                'name': 'Connect to Lambda container',
+                'type': 'rgolang',
+                'request': 'launch',
+                'mode': 'remote',
+                'remotePath': '',
+                'port': 8997,
+                'host': '127.0.0.1',
+                'program': '${workspaceRoot}',
+                'env': {},
+                'args': [],
+                'apiVersion': 1
         }
-
-
     ]
-}" > launch1.json
+}" > launch.json
