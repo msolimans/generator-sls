5a1a320455097954fc67d859ffacfc57232ea623
Muhammad Soliman <msuliman@mum.edu>
2019-02-11 22:53:31 -0500

    python app
    Signed-off-by: Muhammad Soliman <msuliman@mum.edu>

--- generators/apython/index.js
+++ generators/apython/index.js
@@ -3,19 +3,59 @@
 const generators = require("yeoman-generator");
 
 /**
- * Python generator
+ * Python Generator
  */
 const serverGenerator = generators.Base.extend({
 
+
     writing: {
-        tobedone() {
-            this.log("Not finished yet, It will be released soon!");
-        }
+
+        git() {
+            this.fs.copy(
+                this.templatePath("gitattributes"),
+                this.destinationPath(".gitattributes")
+            );
+            this.fs.copy(
+                this.templatePath("gitignore"),
+                this.destinationPath(".gitignore")
+            );
+        },
+
+        readMe() {
+            this.fs.copyTpl(
+                this.templatePath("README.md"),
+                this.destinationPath("README.md"), {
+                    projectName: this.options.props.projectName,
+                    projectDescription: this.options.props.projectDescription,
+                }
+            );
+        },
+
+
+        serverlessYaml() {
+            this.fs.copyTpl(
+                this.templatePath("serverless.yml"),
+                this.destinationPath("serverless.yml"), {
+                    projectName: this.options.props.projectName,
+                }
+            );
+        },
+
+        config() {
+            this.fs.copy(
+                this.templatePath("config.json"),
+                this.destinationPath("config.json")
+            );
+
+            this.fs.copy(
+                this.templatePath("slsattributes.json"),
+                this.destinationPath("slsattributes.json")
+            );
+        },
     },
 
     install() {
-        //this.composeWith("sls:route", {options: {__app: true, language: this.language}});
-        //this.npmInstall();
+        this.composeWith("sls:route", {options: {__app: this.options.props.__app, language: "python"}});
     },
 
 
