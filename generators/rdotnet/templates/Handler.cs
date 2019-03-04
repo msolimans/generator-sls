577c43be16da2f710c0a4bb51694d45818e44b7b
Muhammad Soliman <msuliman@mum.edu>
2019-03-03 21:55:28 -0500

    Fix dotnet proxy responses for API/GW
    Signed-off-by: Muhammad Soliman <msuliman@mum.edu>

--- generators/rdotnet/templates/Handler.cs
+++ generators/rdotnet/templates/Handler.cs
@@ -1,6 +1,9 @@
 using Amazon.Lambda.Core;
 using System;
-
+using Amazon.Lambda.APIGatewayEvents;
+using System.Net;
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
 
 
 namespace AwsDotnetCsharp
@@ -9,7 +12,11 @@ namespace AwsDotnetCsharp
     {
        public <%=Prefix%>Response Run(<%=Prefix%>Request request)
        {
-           return new <%=Prefix%>Response("Go Serverless v1.0! Your function executed successfully!", request);
+         return new APIGatewayProxyResponse
+                   {
+                       Body = JsonConvert.SerializeObject(new <%=Prefix%>Response("Go Serverless v1.0! Your function executed successfully!", request)),
+                         StatusCode = (int) HttpStatusCode.OK,
+                   };
        }
     }
 
