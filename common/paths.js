d54819375157e4564d59f0ccacbcb2508fd1c53e
Muhammad Soliman <msuliman@mum.edu>
2019-05-23 23:08:00 -0400

    common event.json and path helper (basname and dirname)
    Signed-off-by: Muhammad Soliman <msuliman@mum.edu>

---
+++ common/paths.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const pathhelper = {
+    basename(path) {
+        return path.replace(/.*\//, '');
+    },
+    dirname(path) {
+        return path.match(/.*\//);
+    }
+};
+
+module.exports = pathhelper;
