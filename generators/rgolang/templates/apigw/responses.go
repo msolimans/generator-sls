675731dfcd27de8d79a15a20bdfa8f64a00706de
Muhammad Soliman <msuliman@mum.edu>
2019-05-27 01:10:46 -0400

    responses
    Signed-off-by: Muhammad Soliman <msuliman@mum.edu>

--- generators/rgolang/templates/apigw/responses.go
+++ generators/rgolang/templates/apigw/responses.go
@@ -11,39 +11,34 @@ import (
 
 //APIResponse is of type APIGatewayProxyResponse
 type APIResponse events.APIGatewayProxyResponse
+type Headers map[string]string
 
-//Ok marshals response data and returns 200 status code
+//Ok returns response data as text and with 200 status code
 func Ok(data interface{}) (*APIResponse, error) {
-	body, err := json.Marshal(data)
-	if err != nil {
-		return &APIResponse{
-			Body:       fmt.Sprintf("Err Marshaling response: %v", err.Error()),
-			StatusCode: http.StatusOK,
-		}, nil
-	}
-
-	return &APIResponse{
-		Body:       string(body),
-		StatusCode: http.StatusOK,
-	}, nil
+	return ResponseWithHeaders(data, http.StatusOK, nil)
 }
 
-//NotFound accepts message(s) and returns Not Found status code, 404, with all combined passed message(s)
+//Json marshals response data into json and returns it with status code 200
+func Json(data interface{}) (*APIResponse, error) {
+	return ResponseWithHeaders(data, http.StatusOK, Headers{"Content-Type": "application/json"})
+}
+
+//ServerErr returns Not Found code, 404
 func NotFound(message ...string) (*APIResponse, error) {
 	if len(message) > 0 {
-		return Response(strings.Join(message, ","), http.StatusInternalServerError)
+		return Response(fmt.Sprintf(`{"type": "err", "message": "%s"}`, strings.Join(message, ",")), http.StatusNotFound)
 	}
 
-	return Response("Not Found", http.StatusNotFound)
+	return Response(`{"type": "err", "msg": "Not Found"}`, http.StatusNotFound)
 }
 
 //ServerErr returns Internal Server Err code, 500
 func ServerErr(message ...string) (*APIResponse, error) {
 	if len(message) > 0 {
-		return Response(strings.Join(message, ","), http.StatusInternalServerError)
+		return Response(fmt.Sprintf(`{"type": "err", "message": "%s"}`, strings.Join(message, ",")), http.StatusInternalServerError)
 	}
 
-	return Response("Internal Server Error", http.StatusInternalServerError)
+	return Response(`{"type": "err", "msg": "Internal Server Error"}`, http.StatusInternalServerError)
 }
 
 //Response Simplifies responses for Lambda, just pass data and statusCode
@@ -51,29 +46,36 @@ func Response(data interface{}, code int) (*APIResponse, error) {
 	return ResponseWithHeaders(data, code, nil)
 }
 
-//Err Returns error in a specified format
+//Err Returns error in a specified format with specific status code
 func Err(err error, code ...int) (*APIResponse, error) {
-	data := map[string]string{
-		"err": err.Error(),
-	}
-	body, _ := json.Marshal(data)
 	status := http.StatusInternalServerError
 	if len(code) > 0 {
 		status = code[0]
 	}
 
 	return &APIResponse{
-		Body:       string(body),
+		Body:       fmt.Sprintf(`{"type": "err", "msg": "%s"}`, err.Error()),
 		StatusCode: status,
-	}, err
+	}, nil
 }
 
 //ResponseWithHeaders requires all response data members to be filled in
 func ResponseWithHeaders(data interface{}, code int, headers map[string]string, isBase64 ...bool) (*APIResponse, error) {
-	body, err := json.Marshal(data)
-    if err != nil {
-        return Err(err, http.StatusInternalServerError)
-    }
+	var body string
+	if v, ok := data.(string); ok {
+		body = v
+	} else {
+		resp, err := json.Marshal(data)
+		if err != nil {
+			return Err(err, http.StatusInternalServerError)
+		}
+
+		if _, ok := headers["Content-Type"]; !ok {
+			headers["Content-Type"] = "application/json"
+		}
+
+		body = string(resp)
+	}
 
 	base64 := false
 	if len(isBase64) > 0 {
@@ -81,7 +83,7 @@ func ResponseWithHeaders(data interface{}, code int, headers map[string]string, 
 	}
 
 	return &APIResponse{
-		Body:            string(body),
+		Body:            body,
 		StatusCode:      code,
 		IsBase64Encoded: base64,
 		Headers:         headers,
