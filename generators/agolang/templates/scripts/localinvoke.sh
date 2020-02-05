ebd78b3af533bb6f68b592eee5ad150f3c35adff
Muhammad Soliman <m.solimanz@hotmail.com>
2020-02-05 00:40:25 -0500

    local invok and debug

--- generators/agolang/templates/scripts/localinvoke.sh
+++ generators/agolang/templates/scripts/localinvoke.sh
@@ -2,6 +2,13 @@
 
 event="${1}/event.json"
 network=""
+debug=false
+
+if [[ ! -z "${4}" ]]; then
+    echo 'DEBUG is enabled!'
+    debug=true
+fi
+
 if [[ -z "${2}" ]]; then
     echo "No events passed, applying default from ${event}"
 else
@@ -13,12 +20,26 @@ content=$(cat ${event})
 
 if [[ -z "$3" ]]; then
     echo "No docker network specified, applying default docker0"
-    echo "docker run --rm -v $PWD/bin:/var/task lambci/lambda:go1.x ${1} ${content}"
-
-    docker run --rm -v ${PWD}/bin:/var/task lambci/lambda:go1.x ${1} "${content}"
+    
+    if [[ "$debug" = true ]]; then
+        echo "docker run --rm -p 8997:8997 -ti -v $PWD/bin/:/var/task -v $PWD/scripts/linux:/tmp/lambci_debug_files lambci/lambda:go1.x -debug=true -delvePort=8997 route1 ${content}"
+        docker run --rm -p 8997:8997 -ti -v $PWD/bin/:/var/task -v $PWD/scripts/linux:/tmp/lambci_debug_files lambci/lambda:go1.x -debug=true -delvePort=8997 ${1} "${content}"
+    else
+        echo "docker run --rm -v $PWD/bin:/var/task lambci/lambda:go1.x ${1} ${content}"
+        docker run --rm -v ${PWD}/bin:/var/task lambci/lambda:go1.x ${1} "${content}"
+    fi
+    
 else
     network="${3}"
     echo "Docker Network Specified: ${network}"
-    echo "docker run --rm -v $PWD/bin:/var/task --network ${network} lambci/lambda:go1.x $1 ${content}"
-    docker run --rm -v $PWD/bin:/var/task --network ${network} lambci/lambda:go1.x ${1} "${content}"
+    
+    if [[ "$debug" = true ]]; then
+        echo "docker run --rm -p 8997:8997 -ti --network ${network} -v $PWD/bin/:/var/task -v $PWD/scripts/linux:/tmp/lambci_debug_files lambci/lambda:go1.x -debug=true -delvePort=8997 route1 ${content}"
+        docker run --rm -p 8997:8997 -ti --network ${network} -v $PWD/bin/:/var/task -v $PWD/scripts/linux:/tmp/lambci_debug_files lambci/lambda:go1.x -debug=true -delvePort=8997 ${1} "${content}"
+    else
+        echo "docker run --rm -v $PWD/bin:/var/task --network ${network} lambci/lambda:go1.x $1 ${content}"
+        docker run --rm -v $PWD/bin:/var/task --network ${network} lambci/lambda:go1.x ${1} "${content}"
+    fi
+    
 fi
+ 
